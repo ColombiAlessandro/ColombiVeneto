a89d1fc1c1e9b4f937af03323477e595d62330fa
ColombiAlessandro <colombi.alessandro.studente@itispaleocapa.it>
2022-09-28 09:02:44 +0200

    conclusione

--- ColombiVeneto/Form1.Designer.cs
+++ ColombiVeneto/Form1.Designer.cs
@@ -29,31 +29,32 @@ namespace ColombiVeneto
         /// </summary>
         private void InitializeComponent()
         {
-            this.textBox1 = new System.Windows.Forms.TextBox();
-            this.comuneBox = new System.Windows.Forms.Button();
+            this.comuneBox = new System.Windows.Forms.TextBox();
+            this.confermaButton = new System.Windows.Forms.Button();
             this.label1 = new System.Windows.Forms.Label();
             this.SuspendLayout();
             // 
-            // textBox1
-            // 
-            this.textBox1.Location = new System.Drawing.Point(139, 210);
-            this.textBox1.Name = "textBox1";
-            this.textBox1.Size = new System.Drawing.Size(83, 20);
-            this.textBox1.TabIndex = 0;
-            // 
             // comuneBox
             // 
-            this.comuneBox.Location = new System.Drawing.Point(139, 301);
+            this.comuneBox.Location = new System.Drawing.Point(287, 217);
             this.comuneBox.Name = "comuneBox";
-            this.comuneBox.Size = new System.Drawing.Size(83, 23);
-            this.comuneBox.TabIndex = 1;
-            this.comuneBox.Text = "button1";
-            this.comuneBox.UseVisualStyleBackColor = true;
+            this.comuneBox.Size = new System.Drawing.Size(110, 20);
+            this.comuneBox.TabIndex = 0;
+            // 
+            // confermaButton
+            // 
+            this.confermaButton.Location = new System.Drawing.Point(287, 303);
+            this.confermaButton.Name = "confermaButton";
+            this.confermaButton.Size = new System.Drawing.Size(83, 23);
+            this.confermaButton.TabIndex = 1;
+            this.confermaButton.Text = "Conferma";
+            this.confermaButton.UseVisualStyleBackColor = true;
+            this.confermaButton.Click += new System.EventHandler(this.confermaButton_Click);
             // 
             // label1
             // 
             this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(136, 131);
+            this.label1.Location = new System.Drawing.Point(284, 140);
             this.label1.Name = "label1";
             this.label1.Size = new System.Drawing.Size(86, 13);
             this.label1.TabIndex = 2;
@@ -65,8 +66,8 @@ namespace ColombiVeneto
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
             this.Controls.Add(this.label1);
+            this.Controls.Add(this.confermaButton);
             this.Controls.Add(this.comuneBox);
-            this.Controls.Add(this.textBox1);
             this.Name = "Form1";
             this.Text = "Form1";
             this.Load += new System.EventHandler(this.Form1_Load);
@@ -77,8 +78,8 @@ namespace ColombiVeneto
 
         #endregion
 
-        private System.Windows.Forms.TextBox textBox1;
-        private System.Windows.Forms.Button comuneBox;
+        private System.Windows.Forms.TextBox comuneBox;
+        private System.Windows.Forms.Button confermaButton;
         private System.Windows.Forms.Label label1;
     }
 }
--- ColombiVeneto/Form1.cs
+++ ColombiVeneto/Form1.cs
@@ -15,6 +15,8 @@ namespace ColombiVeneto
     public partial class Form1 : Form
     {
         int recordLength;
+        int numeroRighe;
+
         public Form1()
         {
             InitializeComponent();
@@ -25,32 +27,63 @@ namespace ColombiVeneto
             StreamReader sr = new StreamReader(@"veneto_verona.csv");
             recordLength = sr.ReadLine().ToCharArray().Length + 1;
             sr.Close();
-            comuneBox.Text = recordLength.ToString();
             var f = new FileStream(@"veneto_verona.csv", FileMode.Open, FileAccess.ReadWrite);
-            int numeroRighe = (int)(f.Length) / recordLength;
+            numeroRighe = (int)(f.Length) / recordLength;
             numeroRighe += 1;
-            int posizioneNome = 7;
-            int pos;
-            comuneBox.Text = numeroRighe.ToString();
-            BinaryReader reader = new BinaryReader(f);
-            string line;
-            byte[] br;
-            do
-            {
-                pos = numeroRighe / 2;
-                f.Seek(pos * recordLength, SeekOrigin.Begin);
-                br = reader.ReadBytes(recordLength);
-                line = Encoding.ASCII.GetString(br, 0, br.Length);
-                if (line.Split(';')[0] == comuneBox.Text)
-                {
-                    MessageBox.Show(line.Split()[posizioneNome]);
-                }
-                else if (string.Compare(line.Split(';')[0], comuneBox.Text, false))
-                    i = m + 1;
-                else
-                    j = m - 1;
+            f.Close();
+            
+        }
 
-            } while (i <= j && pos == -1);
+        private void confermaButton_Click(object sender, EventArgs e)
+        {
+            var f = new FileStream(@"veneto_verona.csv", FileMode.Open, FileAccess.ReadWrite);
+            if (!string.IsNullOrEmpty(comuneBox.Text) && !string.IsNullOrWhiteSpace(comuneBox.Text))
+            {
+                int posizioneNome = 7;
+                int pos;
+                
+                BinaryReader reader = new BinaryReader(f);
+                string line;
+                byte[] br;
+                int i = 0;
+                int j = numeroRighe - 1;
+                int ctrl = -1;
+                try
+                {
+                    do
+                    {
+                        pos = j + i / 2;
+                        f.Seek(pos * recordLength, SeekOrigin.Begin);
+                        br = reader.ReadBytes(recordLength);
+                        line = Encoding.ASCII.GetString(br, 0, br.Length);
+                        if (line.Split(';')[0] == comuneBox.Text.ToUpper())
+                        {
+                            MessageBox.Show(line.Split(';')[posizioneNome],"Nome struttura:");
+                            f.Close();
+                            return;
+                        }
+                        else if (string.Compare(line.Split(';')[0], comuneBox.Text, false) < 0)
+                        {
+                            i = pos + 1;
+                        }
+                        else
+                            j = pos - 1;
+
+                    } while (i <= j && ctrl == -1);
+                    if (ctrl == -1)
+                    {
+                        MessageBox.Show("Comune non trovato", "Errore");
+                    }
+                }
+                catch (Exception p)
+                {
+                    MessageBox.Show("Comune non trovato", "Errore");
+                }
+            } else
+            {
+                MessageBox.Show("Valore inserito non valido", "Errore");
+            }
+            f.Close();
         }
     }
 }
